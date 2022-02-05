ceae24ed72911a71dc765f983edec0884ac2f939
Dominic Claßen <dominic@dominic-classen.de>
2022-02-05 16:43:46 +0100

    tests: add logout test

--- apps/majesdash-e2e/src/integration/02-login.spec.ts
+++ apps/majesdash-e2e/src/integration/02-login.spec.ts
@@ -1,8 +1,11 @@
-describe('Login with username or email', () => {
-  it('should login with email', () => {
+describe('Login with username or email and can logout', () => {
+  beforeEach(() => {
     cy.clearLocalStorage();
     cy.reload();
     cy.visit('/login');
+  });
+
+  it('should login with email', () => {
     cy.get('input[formcontrolname="emailOrUsername"]').type('test@test.de');
     cy.get('input[formcontrolname="password"]').type('testuser');
     cy.get('button').eq(1).click();
@@ -10,12 +13,19 @@ describe('Login with username or email', () => {
   });
 
   it('should login with username', () => {
-    cy.clearLocalStorage();
-    cy.reload();
-    cy.visit('/login');
     cy.get('input[formcontrolname="emailOrUsername"]').type('testuser');
     cy.get('input[formcontrolname="password"]').type('testuser');
     cy.get('button').eq(1).click();
     cy.get('mat-toolbar').should('contain', 'testuser');
+  });
+
+  it('should login and logout', () => {
+    cy.get('input[formcontrolname="emailOrUsername"]').type('testuser');
+    cy.get('input[formcontrolname="password"]').type('testuser');
+    cy.get('button').eq(1).click();
+    cy.get('mat-toolbar').should('contain', 'testuser');
+    cy.get('button').eq(0).click();
+    cy.get('[data-cy="logout"]').click();
+    cy.get('mat-toolbar').should('not.contain', 'testuser');
   });
 });
