5879bdaf04734de3360ca1c53f541680d8a22267
Dominic Claßen <dominic@dominic-classen.de>
2022-02-15 12:01:44 +0100

    chore: order swagger page after "method"

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -22,7 +22,9 @@ async function bootstrap() {
     )
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: { operationsSorter: 'method' },
+  });
 
   const port = process.env.PORT || 3333;
   await app.listen(port);
